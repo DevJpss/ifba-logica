8aeac1ced73d9e90ba071d98d833ff24c38e207b
João Paulo de Souza Santos <joaopaulodsspessoal@gmail.com>
2025-10-09 14:43:10 -0300

    Create ListaQ5.c

---
+++ ListaQ5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int numero;
+    int sucessor;
+    
+    printf("Digite um numero inteiro\n");
+    scanf("%d",&numero);
+    sucessor = numero + 1;
+    
+    printf("Sucessor: %d ",sucessor);
+   
+}
